9b5f8ec2978e7b48ead5620e4919dd5d9b7e9881
Adarsh-D23 <100186191+Adarsh-D23@users.noreply.github.com>
2022-02-26 20:32:26 +0545

    Email Count and Relevance
    Emails in given txt file were counted and checked for Human and Non-Human email.

---
+++ emailScrap.py
@@ -0,0 +1,50 @@
+import re
+import json
+
+try:
+    with open("websiteData.txt", encoding='utf8') as f:
+        text = f.read()
+        #print(type(text))
+        rel = dict()
+        words = re.split(pattern= r'[,; |\/\n]',string=text)
+
+
+
+
+except FileNotFoundError:
+    text = None
+
+
+def count(mail):
+    count = 0
+    for i in range(0,len(words)-1):
+        if (mail == words[i]):
+            count=count+1
+
+    return count
+
+def check_email(email):
+    n1=email.find('@')
+    text1=email[:n1]
+    pattern1="[a-zA-Z0-9]+\.+[a-zA-Z0-9]"
+    if (re.search(pattern1, text1) or len(text1)>=8):
+        return 'Human'
+    else:
+        return 'Non Human'
+
+
+
+for word in words:
+    pattern= "[a-zA-Z0-9]+@+[a-zA-Z0-9]"
+    if re.search(pattern, word):
+        division=check_email(word)
+        num=count(word)
+        rel.update({word:{'Occurance': num, 'EmailType': division}})
+
+result= json.dumps(rel, indent=1)
+print(result)
+
+with open("result.json", "w") as outfile:
+    json.dump(rel, outfile, indent=4)
+
+
